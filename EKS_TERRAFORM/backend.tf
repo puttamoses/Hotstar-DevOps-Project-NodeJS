b456f6be859aa8d48462e8f173f3fee727b41fe5
Moses putta <136316767+puttamoses@users.noreply.github.com>
2024-08-25 12:06:29 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "projectbucket9866" # Replace with your actual S3 bucket name
+    bucket = "projectbucker9866" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
